4a9df8395e9db6255f86c3b7409483efbe878e02
Mukul Sharma <sh.mukul@gmail.com>
2020-08-12 19:51:29 -0700

    first commit with schema and dir structure

---
+++ db/schema.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+USE employee_db;
+
+CREATE TABLE employee(
+  id INTEGER(11) AUTO_INCREMENT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER(10)  NOT NULL,
+  manager_id INTEGER(10),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE department(
+  id INTEGER(11) AUTO_INCREMENT NOT NULL,
+  department_name VARCHAR(50) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role(
+  id INTEGER(11) AUTO_INCREMENT NOT NULL,
+  title VARCHAR(40) NOT NULL,
+  salary DeCIMAL (10,2) NOT NULL,
+  department_id INTEGER(20) NOT NULL,
+  PRIMARY KEY (id)
+);
